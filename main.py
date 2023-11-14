bec32d7ea91dd987f0f3117017be6fd9a4db5679
emnuelle <rm97973@fiap.com.br>
2023-11-14 17:22:45 -0300

    tentando aq

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def menu_inicial():
         elif escolha == 2:
             usuarios = cadastrar_tutor(usuarios)
         elif escolha == 3:
-            print("Opção de consulta")
+            consultar_pulseira(usuarios)
         elif escolha == 4:
             print("Saindo do sistema. Até logo!")
             break
@@ -43,7 +43,6 @@ def carregar_usuarios():
         return []
     except json.decoder.JSONDecodeError:
         return []
-
 
 def salvar_usuarios(usuarios):
     with open("usuarios.json", "w") as file:
@@ -228,6 +227,8 @@ def cadastrar_tutor(usuarios):
 
     usuarios.append(usuario)
 
+    salvar_usuarios(usuarios)
+
     print("Cadastro realizado com sucesso! Redirecionando para o menu inicial...")
 
 
@@ -271,7 +272,8 @@ def cadastrar_pulseira(nome_tutor, email_tutor, telefone_tutor, rua_tutor, numer
         print(f"Pulseira cadastrada com sucesso! Código: {pulseira['codigo']}")
 
         primeira_pulseira = False
-
+        
+        salvar_usuarios(usuarios)
     return pulseiras
 
 
@@ -460,6 +462,7 @@ def cadastrar_pulseira_individual(nome_tutor, email_tutor, telefone_tutor, rua_t
 
     print("Cadastro realizado com sucesso! Redirecionando para o menu inicial...")
 
+    salvar_usuarios(usuarios)
     # Agora, chame a função menu_inicial para voltar ao menu inicial
     menu_inicial()
 
@@ -493,6 +496,91 @@ def login(usuarios):
                 break
 
             print(f"Você tem mais {3 - tentativas} tentativas restantes. Assim que forem ultrapassadas os sistema irá retornar ao menu inicial.")
+            
+def consultar_pulseira(usuarios):
+    print("\nConsulta de Pulseira")
+    print()
+
+    codigo_pulseira = input("Digite o código da pulseira: ").upper()
+
+    # Search for the pulseira with the given code
+    pulseira_encontrada = None
+    for pulseira in usuarios:
+        if 'codigo' in pulseira and pulseira['codigo'] == codigo_pulseira:
+            pulseira_encontrada = pulseira
+            break
+
+    if pulseira_encontrada:
+        exibir_informacoes_pulseira(pulseira_encontrada)
+    else:
+        print("Pulseira não encontrada. Verifique o código e tente novamente.")
+
+def exibir_informacoes_pulseira(pulseira):
+    print("\nInformações da Pulseira")
+    print(f"Código da Pulseira: {pulseira['codigo']}")
+    print(f"Nome do Usuário: {pulseira['nome']}")
+    print(f"Data de Nascimento: {pulseira['nascimento']}")
+    print(f"Tipo Sanguíneo: {pulseira['tipo_sanguineo']}")
+    
+    # Display other information as needed
+    
+    # Display conditions médicas
+    print("\nCondições Médicas:")
+    for condicao in pulseira['condicoes_medicas']:
+        print(f"- {condicao}")
+
+    # Display ocorrências médicas
+    print("\nOcorrências Médicas:")
+    for ocorrencia in pulseira['ocorrencias_medicas']:
+        print(f"Data: {ocorrencia['Data']}")
+        print(f"Local: {ocorrencia['Local']}")
+        print(f"Relato: {ocorrencia['Relato']}")
+        print(f"Sintomas: {ocorrencia['Sintomas']}")
+        print("Medicamentos Prescritos:")
+        for medicamento in ocorrencia['Medicamentos']:
+            print(f"  - Nome: {medicamento['Nome']}")
+            print(f"    Tipo: {medicamento['Tipo']}")
+            print(f"    Quantidade: {medicamento['Quantidade']}")
+            print(f"    Tempo de Consumo: {medicamento['Tempo']}")
+        print()
+
+    # Display alergias
+    print("\nAlergias:")
+    for alergia in pulseira['alergias']:
+        print(f"- {alergia['Alergia']}")
+        if 'Especificacao' in alergia and alergia['Especificacao']:
+            print(f"  Especificação: {alergia['Especificacao']}")
+
+    # Display vacinas recentes
+    print("\nVacinas Recentes:")
+    print(f"{pulseira['vacinas_recentes']['titulo']}")
+    print(f"{pulseira['vacinas_recentes']['descricao']}")
+    for vacina in pulseira['vacinas_recentes']['vacinas']:
+        print(f"  - Nome: {vacina['nome']}")
+        print(f"    Data: {vacina['data']}")
+        print(f"    Descrição: {vacina['descricao']}")
+        print(f"    Local: {vacina['local']}")
+    
+    # Display medicamentos
+    print("\nMedicamentos:")
+    for medicamento in pulseira['medicamentos']:
+        print(f"Nome: {medicamento['Nome']}")
+        print(f"Tipo: {medicamento['Tipo']}")
+        print(f"Quantidade: {medicamento['Quantidade']}")
+        print(f"Tempo de Consumo: {medicamento['Tempo']}")
+        print()
+
+    # Display informações do tutor
+    print("\nInformações do Tutor:")
+    print(f"Nome: {pulseira['usuario']['nome_tutor']}")
+    print(f"E-mail: {pulseira['usuario']['email_tutor']}")
+    print(f"Telefone: {pulseira['usuario']['telefone_tutor']}")
+    print(f"Endereço: {pulseira['usuario']['rua_tutor']}, {pulseira['usuario']['numero_tutor']}")
+    print(f"CEP: {pulseira['usuario']['cep_tutor']}")
+    print(f"Bairro: {pulseira['usuario']['bairro_tutor']}")
+    print(f"Cidade: {pulseira['usuario']['cidade_tutor']}")
+    print(f"Estado: {pulseira['usuario']['estado_tutor']}")
+    print(f"Informações Adicionais: {pulseira['usuario']['informacoes_adicionais_tutor']}")
 
 if __name__ == "__main__":
     menu_inicial()
